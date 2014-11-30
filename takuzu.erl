8c1c635940e6e8a2e00df69599461ad04e30964f
Dmitry Balakhonskiy <dnomaid@gmail.com>
2014-12-01 01:04:39 +0300

    optimize duplicates check

--- takuzu.erl
+++ takuzu.erl
@@ -13,6 +13,8 @@
 -export([generate/1, generate/2, generate_print/2, measure/2]).
 -compile([export_all]).
 
+%% -record(field, {flat, rows, columns}).
+-record(cell, {x, y}).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Публичные функции
@@ -121,17 +123,18 @@ remove_suggestions(Field, Suggestions) ->
 check_field(Field, Parallel) -> check_field(Field, Parallel, false).
 check_field(Field, Parallel, CheckFilled) ->
   Size = field_size(Field),
+  Rows = get_rows(Field, Size),
+  Columns = get_columns(Rows),
   apply(case Parallel of true -> fun pmap/2; _ -> fun lists:map/2 end,
-    [fun(N) -> check_cell(Field, N, Size, CheckFilled) end, lists:seq(1, Size * Size)]).
+    [fun(N) -> check_cell(Field, Rows, Columns, N, Size, CheckFilled) end, lists:seq(1, Size * Size)]).
 
-%% Проверяет ячейку на все условия, возвращает первый найденный повод заполнить ее, false или filled для уже заполненных
-check_cell(Field, N, Size) -> check_cell(Field, N, Size, false).
-check_cell(Field, N, Size, CheckFilled) ->
+check_cell(Field, Rows, Columns, N, Size) -> check_cell(Field, Rows, Columns, N, Size, false).
+check_cell(Field, Rows, Columns, N, Size, CheckFilled) ->
   Funs = [
-    {x, fun check_twins/3},
-    {x, fun check_pairs/3},
-    {x, fun check_others/3},
-    {x, fun check_duplicates/3}
+    {x, fun(FF, NN, SS) -> check_twins(FF, NN, SS) end},
+    {x, fun(FF, NN, SS) -> check_pairs(FF, NN, SS) end},
+    {x, fun(FF, NN, SS) -> check_others(FF, NN, SS) end},
+    {x, fun(_, NN, SS) -> check_duplicates(Rows, Columns, NN, SS) end}
   ],
   IsFilled = lists:nth(N, Field) /= 0,
   if
@@ -168,7 +171,10 @@ check_twins(Field, N, Size) ->
 check_others(Field, N, Size) ->
   Funs = [
     {row, fun get_row_others/3},
-    {column, fun get_column_others/3}],
+    {column, fun get_column_others/3}
+%%     {row, fun(_, NN, SS)-> remove_nth(get_xpos(NN,SS), lists:nth(get_ypos(NN, SS), Rows)) end},
+%%     {column, fun(_, NN, SS)-> remove_nth(get_ypos(NN,SS), lists:nth(get_xpos(NN, SS), Columns)) end}
+  ],
   check_until(Funs, [Field, N, Size],
     fun(Res, Tag) ->
       Ones = count_elements(Res, 1),
@@ -180,33 +186,21 @@ check_others(Field, N, Size) ->
       end
     end).
 
-check_duplicate_row(Field, N, Size) ->
-  Row = get_row(Field, N, Size),
+check_duplicate_row(Rows, #cell{x = X, y = Y}, Tag) ->
+  Row = lists:nth(Y, Rows),
   case count_elements(Row, 0) of
     2 ->
-      XPos = get_xpos(N, Size),
-      case find_duplicate_row(Field, Row, Size) of
+      case find_duplicate_row(Rows, Row) of
         false -> false;
-        Res -> {duplicate, 3 - lists:nth(XPos, Res), row}
+        Res -> {duplicate, 3 - lists:nth(X, Res), Tag}
       end;
     _ -> false
   end.
 
-check_duplicate_column(Field, N, Size) ->
-  Column = get_column(Field, N, Size),
-  case count_elements(Column, 0) of
-    2 ->
-      YPos = get_ypos(N, Size),
-      case find_duplicate_column(Field, Column, Size) of
-        false -> false;
-        Res -> {duplicate, 3 - lists:nth(YPos, Res), column}
-      end;
-    _ -> false
-  end.
-
-check_duplicates(Field, N, Size) ->
-  case check_duplicate_row(Field, N, Size) of
-    false -> check_duplicate_column(Field, N, Size);
+check_duplicates(Rows, Columns, N, Size) ->
+  Cell = make_cell(nsize, N, Size),
+  case check_duplicate_row(Rows, Cell, row) of
+    false -> check_duplicate_row(Columns, transpose_cell(Cell), column);
     Res -> Res
   end.
 
@@ -277,6 +271,15 @@ get_twins_horizontal(Field, N, _) ->
 
 %% Наполовину заполненные строки и колонки
 
+get_rows(Field, Size) -> get_rows([], Field, Size).
+get_rows(Acc, [], _) -> lists:reverse(Acc);
+get_rows(Acc, Field, Size) ->
+  {Row, TailRows} = lists:split(Size, Field),
+  get_rows([Row | Acc], TailRows, Size).
+
+get_columns(Field, Size) -> get_columns(get_rows(Field, Size)).
+get_columns(FieldRows) -> transpose(FieldRows).
+
 get_row_others(Field, N, Size) ->
   RowStart = Size * trunc((N - 1) / Size) + 1,
   lists:sublist(Field, RowStart, N - RowStart) ++ lists:sublist(Field, N + 1, RowStart + Size - N - 1).
@@ -292,41 +295,19 @@ get_column_fun(_, {{N, Size, Cur, Skip}, Acc}) -> {{N, Size, Cur + 1, Skip}, Acc
 
 %% Поиск дублирующихся строк и колонок
 
-get_row(Field, N, Size) ->
-  RowStart = Size * trunc((N - 1) / Size) + 1,
-  lists:sublist(Field, RowStart, Size).
-
-get_column(Field, N, Size) ->
-  {_, Res} = lists:foldl(fun get_column_fun/2, {{N rem Size, Size, 1, -1}, []}, Field),
-  lists:reverse(Res).
-
 can_be_same([], []) -> true;
 can_be_same([H | T1], [H | T2]) -> can_be_same(T1, T2);
 can_be_same([0 | T1], [_ | T2]) -> can_be_same(T1, T2);
 can_be_same([_ | T1], [0 | T2]) -> can_be_same(T1, T2);
 can_be_same(_, _) -> false.
 
-find_duplicate_row([], _, _) -> false;
-find_duplicate_row(Field, SampleRow, Size) ->
-  Row = lists:sublist(Field, 1, Size),
+find_duplicate_row([], _) -> false;
+find_duplicate_row([Row | Rows], SampleRow) ->
   IsFull = 0 == count_elements(Row, 0),
   CanBeSame = can_be_same(SampleRow, Row),
   case IsFull and CanBeSame of
     true -> Row;
-    false -> find_duplicate_row(lists:nthtail(Size, Field), SampleRow, Size)
-  end.
-
-find_duplicate_column(Field, Sample, Size) ->
-  find_duplicate_column(Size, Field, Sample, Size).
-
-find_duplicate_column(0, _, _, _) -> false;
-find_duplicate_column(N, Field, Sample, Size) when N > 0 ->
-  Column = get_column(Field, N, Size),
-  IsFull = 0 == count_elements(Column, 0),
-  CanBeSame = can_be_same(Sample, Column),
-  case IsFull and CanBeSame of
-    true -> Column;
-    false -> find_duplicate_column(N - 1, Field, Sample, Size)
+    false -> find_duplicate_row(Rows, SampleRow)
   end.
 
 
@@ -366,9 +347,17 @@ pmap_gather([Pid | T], Ref) ->
   end;
 pmap_gather([], _) -> [].
 
+transpose([[] | _]) -> [];
+transpose(M) ->
+  [lists:map(fun hd/1, M) | transpose(lists:map(fun tl/1, M))].
+
 set_nth(N, List, Val) when N > 0 -> set_nth([], N, List, Val).
 set_nth(Acc, 1, [_ | T], Val) -> lists:reverse(Acc, [Val | T]);
 set_nth(Acc, N, [H | T], Val) -> set_nth([H | Acc], N - 1, T, Val).
+
+remove_nth(N, List) when N >= 1 -> remove_nth([], N, List).
+remove_nth(Acc, 1, [_ | T]) -> lists:reverse(Acc, T);
+remove_nth(Acc, N, [H | T]) -> remove_nth([H | Acc], N - 1, T).
 
 find_all(Val, List) ->
   {_, RRes} = lists:foldl(fun(I, {N, Acc}) ->
@@ -379,6 +368,10 @@ find_all(Val, List) ->
 %% Считает в списке количество элементов, равных указанному
 count_elements(List, E) ->
   lists:foldl(fun(I, Acc) -> if I == E -> Acc + 1; true -> Acc end end, 0, List).
+
+make_cell(nsize, N, Size) -> #cell{x = get_xpos(N, Size), y = get_ypos(N, Size)}.
+
+transpose_cell(C) -> #cell{x = C#cell.y, y = C#cell.x}.
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -469,8 +462,6 @@ get_others_test() ->
     9, a, b, c,
     d, e, f, g
   ],
-  ?assertEqual([5, 6, 7, 8], get_row(Field, 7, 4)),
-  ?assertEqual([3, 7, b, f], get_column(Field, 7, 4)),
   ?assertEqual([2, 3, 4], get_row_others(Field, 1, 4)),
   ?assertEqual([5, 7, 8], get_row_others(Field, 6, 4)),
   ?assertEqual([9, a, b], get_row_others(Field, 12, 4)),
@@ -510,17 +501,19 @@ check_duplicate_row_test() ->
     2, 0, 0, 2, 0, 0
   ],
   Size = field_size(Field),
-  ?assertEqual([2, 1, 1, 2, 2, 1], find_duplicate_row(Field, [2, 1, 0, 2, 0, 1], Size)),
-  ?assertEqual(false, find_duplicate_row(Field, [1, 1, 0, 2, 0, 1], Size)),
-  ?assertEqual([2, 2, 1, 1, 2, 2], find_duplicate_column(Field, [2, 2, 0, 1, 0, 2], Size)),
-  ?assertEqual(false, find_duplicate_column(Field, [2, 0, 2, 1, 0, 1], Size)),
-  ?assertEqual({duplicate, 2, row}, check_duplicates(Field, 9, Size)),
-  ?assertEqual({duplicate, 1, row}, check_duplicates(Field, 11, Size)),
-  ?assertEqual({duplicate, 2, column}, check_duplicates(Field, 13, Size)),
-  ?assertEqual({duplicate, 1, column}, check_duplicates(Field, 25, Size)),
-  ?assertEqual(false, check_duplicates(Field, 21, Size)).
+  Rows = get_rows(Field, Size),
+  Columns = get_columns(Rows),
+  ?assertEqual([2, 1, 1, 2, 2, 1], find_duplicate_row(Rows, [2, 1, 0, 2, 0, 1])),
+  ?assertEqual(false, find_duplicate_row(Rows, [1, 1, 0, 2, 0, 1])),
+  ?assertEqual([2, 2, 1, 1, 2, 2], find_duplicate_row(Columns, [2, 2, 0, 1, 0, 2])),
+  ?assertEqual(false, find_duplicate_row(Columns, [2, 0, 2, 1, 0, 1])),
+  ?assertEqual({duplicate, 2, row}, check_duplicates(Rows, Columns, 9, Size)),
+  ?assertEqual({duplicate, 1, row}, check_duplicates(Rows, Columns, 11, Size)),
+  ?assertEqual({duplicate, 2, column}, check_duplicates(Rows, Columns, 13, Size)),
+  ?assertEqual({duplicate, 1, column}, check_duplicates(Rows, Columns, 25, Size)),
+  ?assertEqual(false, check_duplicates(Rows, Columns, 21, Size)).
 
-check_cell_test() ->
+check_cell2_test() ->
   Field = [
     0, 1, 0, 1, 0, 0,
     0, 1, 1, 0, 0, 0,
@@ -530,14 +523,16 @@ check_cell_test() ->
     0, 1, 1, 0, 0, 0
   ],
   Size = field_size(Field),
-  ?assertEqual(false, check_cell(Field, 1, Size)),
-  ?assertEqual({twins, 2, vertical}, check_cell(Field, 14, Size)),
-  ?assertEqual({pair, 1, below}, check_cell(Field, 15, Size)),
-  ?assertEqual({pair, 2, right}, check_cell(Field, 31, Size)),
-  ?assertEqual({pair, 2, left}, check_cell(Field, 34, Size)),
-  ?assertEqual({twins, 2, horizontal}, check_cell(Field, 3, Size)),
-  ?assertEqual(false, check_cell(Field, 28, Size)),
-  ?assertEqual(filled, check_cell(Field, 8, Size)),
+  Rows = get_rows(Field, Size),
+  Columns = get_columns(Rows),
+  ?assertEqual(false, check_cell(Field, Rows, Columns, 1, Size)),
+  ?assertEqual({twins, 2, vertical}, check_cell(Field, Rows, Columns, 14, Size)),
+  ?assertEqual({pair, 1, below}, check_cell(Field, Rows, Columns, 15, Size)),
+  ?assertEqual({pair, 2, right}, check_cell(Field, Rows, Columns, 31, Size)),
+  ?assertEqual({pair, 2, left}, check_cell(Field, Rows, Columns, 34, Size)),
+  ?assertEqual({twins, 2, horizontal}, check_cell(Field, Rows, Columns, 3, Size)),
+  ?assertEqual(false, check_cell(Field, Rows, Columns, 28, Size)),
+  ?assertEqual(filled, check_cell(Field, Rows, Columns, 8, Size)),
   Field2 = [
     1, 0, 0, 1, 0, 0,
     2, 1, 0, 2, 0, 1,
@@ -547,10 +542,12 @@ check_cell_test() ->
     2, 1, 1, 2, 2, 1
   ],
   Size2 = field_size(Field2),
-  ?assertEqual({duplicate, 2, row}, check_cell(Field2, 9, Size2)),
-  ?assertEqual({duplicate, 1, row}, check_cell(Field2, 11, Size2)),
-  ?assertEqual({duplicate, 1, column}, check_cell(Field2, 13, Size2)),
-  ?assertEqual({duplicate, 2, column}, check_cell(Field2, 19, Size2)).
+  Rows2 = get_rows(Field2, Size2),
+  Columns2 = get_columns(Rows2),
+  ?assertEqual({duplicate, 2, row}, check_cell(Field2, Rows2, Columns2, 9, Size2)),
+  ?assertEqual({duplicate, 1, row}, check_cell(Field2, Rows2, Columns2, 11, Size2)),
+  ?assertEqual({duplicate, 1, column}, check_cell(Field2, Rows2, Columns2, 13, Size2)),
+  ?assertEqual({duplicate, 2, column}, check_cell(Field2, Rows2, Columns2, 19, Size2)).
 
 set_nth_test() ->
   ?assertEqual([1, 2, 3, a], set_nth(4, [1, 2, 3, 4], a)),
@@ -567,5 +564,15 @@ get_xypos_test() ->
 
 apply_suggestions_test() ->
   ?assertEqual([0, 0, 2, 0], apply_suggestions([0, 0, 0, 0], [false, filled, {pair, 2, below}, false])).
+
+get_rows_test() ->
+  ?assertEqual([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]], get_rows([1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4], 4)).
+
+get_columns_test() ->
+  ?assertEqual([[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]], get_columns([1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4], 4)).
+
+remove_nth_test() ->
+  ?assertEqual([2, 3, 4], remove_nth(1, [1, 2, 3, 4])),
+  ?assertEqual([1, 2, 4], remove_nth(3, [1, 2, 3, 4])).
 
 -endif.
